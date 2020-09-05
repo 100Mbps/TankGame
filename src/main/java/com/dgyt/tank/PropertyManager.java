59d332c01f7e6a45da0a2dd4a9c9717b763817a3
rongjie Han <hanrongjie2011@gmail.com>
2020-09-05 17:20:06 +0800

    单例模式

--- src/main/java/com/dgyt/tank/PropertyManager.java
+++ src/main/java/com/dgyt/tank/PropertyManager.java
@@ -1,12 +1,11 @@
 package com.dgyt.tank;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyManager {
     private static final Properties props = new Properties();
+    private PropertyManager(){}
 
     static {
         try {
